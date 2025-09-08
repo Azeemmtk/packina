35ebe68545768d73b988699bd70983ae34be302d
Azeemmtk <aseemmtk@gmail.com>
2025-09-08 13:58:46 +0530

    report compleated

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -2,5 +2,5 @@ class AppState {
   static bool isAdmin = false;
   static const String adminUid = 'admin_fixed_uid';
   static const String adminDisplayName = 'Admin';
-  static const String adminPhotoURL = 'https://example.com/admin-photo.jpg';
+  static const String adminPhotoURL = 'https://st2.depositphotos.com/1002277/10073/i/450/depositphotos_100732302-stock-photo-word-admin-on-wood-planks.jpg';
 }
